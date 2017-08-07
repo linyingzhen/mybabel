cb1a73d264d9caabee690b5a867311a3e965d200
linyingzhen <277715645@qq.com>
2017-08-07 09:19:45 +0800

    2017080791942

---
+++ a.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log('script');
+console.log('script1');
+console.log('script2');
+console.log('script3');
+
+//# sourceMappingURL=a.js.map
---
+++ script-compiled.js
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log('x');
+'use strict';
+
+console.log('y');
